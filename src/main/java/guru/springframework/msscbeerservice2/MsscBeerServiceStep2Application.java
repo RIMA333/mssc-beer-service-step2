7112cb0d887a9f8cb2f05957bff0cbe17644d4af
Riccardo <riccardo.martella@gmail.com>
2023-10-09 11:31:21 +0200

    Firt release only project tree

---
+++ src/main/java/guru/springframework/msscbeerservice2/MsscBeerServiceStep2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceStep2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceStep2Application.class, args);
+	}
+
+}
